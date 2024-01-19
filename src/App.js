f273e082989e75e8815372950e305e3c5895d624
Frank Leons <dev.frinno@gmail.com>
2024-01-19 12:21:19 +0000

    #UI Change

--- src/App.js
+++ src/App.js
@@ -10,31 +10,26 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">DarasaTech</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+    
+            <a href="https://github.com/frinnolab" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
             </a>
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>We cant wait to meet you.</h1>
+            <p>We are currently under construction . Please check back often .</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:dev.frinno@gmail.com">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          
         </div>
       </div>
     );
